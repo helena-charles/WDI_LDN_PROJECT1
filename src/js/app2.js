0bd845f7d3138918499a3e9fa28b682936ebe2d7
Helena Charles <helsz@hotmail.co.uk>
2018-02-13 23:00:20 +0000

    Fixing bugs, updating game logic for hangman section

--- src/js/app2.js
+++ src/js/app2.js
@@ -12,32 +12,38 @@ $(() => {
     {
       letters: ['c','a','t'],
       incorrectLetters: ['b', 'g', 'o'],
+      alreadyFoundCorrectLetters: [],
       hint: 'Likes milk.'
     },
     {
       letters: ['f','i','s','h'],
       incorrectLetters: ['b', 'g', 'o'],
+      alreadyFoundCorrectLetters: [],
       hint: 'Lives in a bowl, short memory span.'
     },
     {
       letters: ['r','a','b','b','i','t'],
       incorrectLetters: ['b', 'g', 'o'],
+      alreadyFoundCorrectLetters: [],
       hint: 'Likes carrots, twitchy nose.'
     },
     {
       letters: ['h','a','m','s','t','e','r'],
       incorrectLetters: ['b', 'g', 'o'],
+      alreadyFoundCorrectLetters: [],
       hint: 'Runs in a wheel, sleeps in hay.'
     },
     {
       letters: ['d','o','g'],
       incorrectLetters: ['p', 'e', 'l'],
-      hint: 'The goodest boy ever.'
+      alreadyFoundCorrectLetters: [],
+      hint: 'Man\'s best friend.'
     }
   ];
 
   const randomWordFinder = Math.floor(Math.random() * wordsArray.length);
   const randomWordCorrect = wordsArray[randomWordFinder].letters;
+  const randomWordAlreadyFoundCorrect = wordsArray[randomWordFinder].alreadyFoundCorrectLetters;
   const randomWordIncorrect = wordsArray[randomWordFinder].incorrectLetters;
   console.log('the random word is: ' + randomWordCorrect);
 
@@ -45,15 +51,18 @@ $(() => {
   console.log(wordHint);
   $hint.text(wordHint);
 
-  const letters = randomWordCorrect;
   const incorrectLetters = randomWordIncorrect;
   // let lastLetter;
 
   function placeRandomLetter() {
+    const letters = randomWordCorrect.filter(l => randomWordAlreadyFoundCorrect.indexOf(l) === -1);
     const randomCell = Math.floor(Math.random() * 100);
     const letterIndex = Math.floor(Math.random() * letters.length);
     const randomCell2 = Math.floor(Math.random() * 100);
     const incorrectLetterIndex = Math.floor(Math.random() * incorrectLetters.length);
+    cells[randomCell].innerHTML = letters[letterIndex];
+    cells[randomCell].classList.add('pink');
+
     letters.forEach((letter, i) => {
       if (letterIndex === i) {
         cells[randomCell].innerHTML = letter;
@@ -85,27 +94,42 @@ $(() => {
     guess.innerHTML = '-';
   });
 
+  const guesses = document.querySelectorAll('.guess');
 
   function clickEvent() {
     cells.forEach((cell) => {
       if ($(cell).hasClass('pink')) {
         $(cell).on('click', (e) => {
+
           const text = e.target.innerHTML;
           currentLetters.push(text);
           $(e.target).removeClass('pink');
           e.target.innerHTML = '';
-          placeRandomLetter();
           $(cell).off('click');
           console.log('the word array is: ' + currentLetters);
           const indexInWord = randomWordCorrect.indexOf(text);
-          const guesses = document.querySelectorAll('.guess');
           if (indexInWord !== -1) {
+            randomWordAlreadyFoundCorrect.push(text);
             guesses[indexInWord].textContent = text;
           }
+          if ( Array.from($('.wordHolder ul li')).map(node => node.innerText).join('') === randomWordCorrect.join('')) {
+            return;
+          }
+          $cells.text('');
+          placeRandomLetter();
         });
       }
     });
   }
 
+  const $playAgainBtn = $('.playAgainBtn');
+
+  $playAgainBtn.on('click', () => {
+    $cells.html('');
+    $(guesses).html('-');
+    console.log('play again');
+    placeRandomLetter();
+    clickEvent();
+  });
 
 });
